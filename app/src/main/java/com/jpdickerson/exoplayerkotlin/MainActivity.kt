6cda5a2898ad4e446b3ec1b6dc6d651c9ed2bd9b
jpdickerson <justin.p.dickerson@gmail.com>
2020-09-24 15:43:02 -0400

    Refactored code to utilize the new mediaItem type and updated Kotlin and Exo-Player versions to latest & greatest

--- app/src/main/java/com/jpdickerson/exoplayerkotlin/MainActivity.kt
+++ app/src/main/java/com/jpdickerson/exoplayerkotlin/MainActivity.kt
@@ -10,13 +10,20 @@ import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
+import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaUri: Uri
+    private lateinit var mediaItem: MediaItem
+    private lateinit var mediaSource: MediaSource
+    private lateinit var userAgent: DefaultDataSourceFactory
 
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,31 +34,43 @@ class MainActivity : AppCompatActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.M)
-    private fun exoplayer(){
+    private fun exoplayer() {
 //        val trackSelector = DefaultTrackSelector() // old way
 //        val trackSelector = DefaultTrackSelector(baseContext) // requires a context, un-used
 //        val exoPlayer = ExoPlayerFactory.newSimpleInstance(baseContext, trackSelector) // old way
-        val exoPlayer = SimpleExoPlayer.Builder(baseContext).build() // uses builder instead of factory
+        val exoPlayer =
+            SimpleExoPlayer.Builder(baseContext).build() // uses builder instead of factory
         epPlayerView?.player = exoPlayer
         mediaSessionPlayback()
 
-        if (internet()){
-            val mediaUri = Uri.parse(baseContext.getString(R.string.fireside_s)) // via server
-            val userAgent = DefaultDataSourceFactory(
+        if (internet()) {
+            mediaUri = Uri.parse(baseContext.getString(R.string.fireside_s)) // via server
+            userAgent = DefaultDataSourceFactory(
                 baseContext,
                 Util.getUserAgent(baseContext, R.string.jd_musicplayer.toString())
             )
-            val mediaSource = ProgressiveMediaSource.Factory(userAgent).createMediaSource(mediaUri)
-            exoPlayer.prepare(mediaSource)
+            /*New way of setting mediaSource via mediaItem, using the builder pattern*/
+            val mediaItem = MediaItem.Builder()
+                .setUri(mediaUri)
+//                    sets clipping of media item
+                .setClipStartPositionMs(9000)
+                .setClipEndPositionMs(23500)
+                .build()
+            mediaSource = DefaultMediaSourceFactory(userAgent).createMediaSource(mediaItem)
+            exoPlayer.setMediaSource(mediaSource)
+            exoPlayer.prepare()
 
         } else {
-            val mediaUri = Uri.parse(AssetUriConstants.fireside) // via file
-            val userAgent = DefaultDataSourceFactory(
+            mediaUri = Uri.parse(AssetUriConstants.fireside) // via file
+            userAgent = DefaultDataSourceFactory(
                 baseContext,
                 Util.getUserAgent(baseContext, R.string.jd_musicplayer.toString())
             )
-            val mediaSource = ProgressiveMediaSource.Factory(userAgent).createMediaSource(mediaUri)
-            exoPlayer.prepare(mediaSource)
+//            Simple mediaItem from a single Uri
+            mediaItem = MediaItem.fromUri(mediaUri)
+            mediaSource = ProgressiveMediaSource.Factory(userAgent).createMediaSource(mediaItem)
+            exoPlayer.setMediaSource(mediaSource)
+            exoPlayer.prepare()
         }
     }
 
